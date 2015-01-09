ad30dba8bbd5b3a8211f0b44cae8dbc089932ea4
Daniel Sanders <daniel.sanders@imgtec.com>
2015-01-09 10:54:22 +0000

    MIPS: Changed current_thread_info() to an equivalent supported by both clang and GCC
    Without this, a 'break' instruction is executed very early in the boot and
    the boot hangs.
    
    The problem is that clang doesn't honour named registers on local variables
    and silently treats them as normal uninitialized variables. However, it
    does honour them on global variables.
    
    Signed-off-by: Daniel Sanders <daniel.sanders@imgtec.com>

--- arch/mips/include/asm/thread_info.h
+++ arch/mips/include/asm/thread_info.h
@@ -58,11 +58,11 @@ struct thread_info {
 #define init_stack		(init_thread_union.stack)
 
 /* How to get the thread information struct from C.  */
+register struct thread_info *current_gp_register asm("$28");
+
 static inline struct thread_info *current_thread_info(void)
 {
-	register struct thread_info *__current_thread_info __asm__("$28");
-
-	return __current_thread_info;
+	return current_gp_register;
 }
 
 #endif /* !__ASSEMBLY__ */
