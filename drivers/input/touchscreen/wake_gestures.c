ab57cd80a2c480b2d2b27cea3a7938a9afdcd7ef
DespairFactor <matthewdalex92@gmail.com>
2016-03-13 03:03:24 -0400

    wake_gestures: drop timer for double tap to 75HZ

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -54,11 +54,11 @@
 #define SWEEP_X_FINAL           360
 #define SWEEP_Y_NEXT            180
 #define DT2W_FEATHER		150
-#define DT2W_TIME 		150
+#define DT2W_TIME 		75
 
 /* Wake Gestures */
 #define SWEEP_TIMEOUT		90
-#define TRIGGER_TIMEOUT		150
+#define TRIGGER_TIMEOUT		75
 #define WAKE_GESTURE		0x0b
 #define SWEEP_RIGHT		0x01
 #define SWEEP_LEFT		0x02
@@ -118,7 +118,7 @@ static void report_gesture(int gest)
 
 	if (pwrtrigger_time[0] - pwrtrigger_time[1] < TRIGGER_TIMEOUT)
 		return;
-	wake_lock_timeout(&dt2w_wakelock, 150);
+	wake_lock_timeout(&dt2w_wakelock, 75);
 	input_report_rel(gesture_dev, WAKE_GESTURE, gest);
 	input_sync(gesture_dev);
 }
@@ -196,7 +196,7 @@ static void new_touch(int x, int y) {
 	x_pre = x;
 	y_pre = y;
 	touch_nr++;
-	wake_lock_timeout(&dt2w_wakelock, 150);
+	wake_lock_timeout(&dt2w_wakelock, 75);
 }
 
 /* Doubletap2wake main function */
